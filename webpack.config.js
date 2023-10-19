1d86e523f3b1ad9943f8e03e76e4480dbf056bdc
Vadzim Aleshchyk <vadzim.aleshchyk@lseg.com>
2023-10-19 21:54:53 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    watch: true,
+    watch: false,
     entry: ['./index.js', './styles/main.css'],
     output: {
         path: path.join(__dirname, '/dist'),
